2dc059fcff70dcfa664cb9dfcb829c9065e93600
Manuel Vargas <manuelv24@ausdk12.org>
2023-10-06 12:34:01 -0700

    Week8Day4

--- cam-v1/RunnerMain.java
+++ cam-v1/RunnerMain.java
@@ -1,9 +1,36 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+ import javax.swing.*;
+
 public class RunnerMain{
-    public static void main(String[] args) {
-        VirtualPet v = new VirtualPet();
-        v.feed();
-        v.sleep();
+    public RunnerMain(){
+        VirtualPet p = new VirtualPet();
+        p.exercise();
+        takeABeat(1000);
+        p.feed();
 
     }
+    public String getAnswer(String q){
+        String s = (String)JOptionPane.showInputDialog(
+                    new JFrame(),
+                    "Your Message",
+                    "Your Title",
+                    JOptionPane.PLAIN_MESSAGE
+);
+
+    }
+    public void takeABeat(int milliseconds){
+        try {
+                Thread.sleep(1000); //milliseconds
+            } catch(Exception e){
+        
+            }
+        }
+
+
+ public static void main(String[] args) {
+    RunnerMain r = new RunnerMain();
+
+ }
 
 }
